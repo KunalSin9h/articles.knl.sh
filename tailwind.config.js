dc88ac5d3b362aa9538a1e2b8b88430b66e0583f
Kunal Singh <kunalsin9h@gmail.com>
2022-12-08 23:48:22 +0530

    completed home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/**/*.{js, ts, jsx, tsx}", "./src/app/**"],
+  content: [
+    "./src/app/**/*.{js, ts, jsx, tsx}",
+    "./src/app/**",
+    "./src/components/**/*.{js, jsx, ts, tsx}",
+    "./src/components/**",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      blackBg: "#171717",
+      whiteBg: "#EEEED0",
+      blackText: "#181815",
+      whiteText: "#E8E8E8",
+      testRed: "red",
+    },
   },
   plugins: [],
+  // darkMode: "class",
 };
