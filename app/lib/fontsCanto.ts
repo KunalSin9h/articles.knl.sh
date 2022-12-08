038cfb457159ab874f59545532f8fe266e696088
Kunal Singh <kunalsin9h@gmail.com>
2022-12-08 15:03:08 +0530

    canto font added

---
+++ app/lib/fontsCanto.ts
@@ -0,0 +1,38 @@
+import localFont from "@next/font/local";
+
+const cantoBold = localFont({
+  src: "../../fonts/canto/Canto-Bold.woff2",
+});
+
+const cantoBoldItalic = localFont({
+  src: "../../fonts/canto/Canto-BoldItalic.woff2",
+});
+
+const cantoItalic = localFont({ src: "../../fonts/canto/Canto-Italic.woff2" });
+
+const cantoLight = localFont({ src: "../../fonts/canto/Canto-Light.woff2" });
+
+const cantoLightItalic = localFont({
+  src: "../../fonts/canto/Canto-LightItalic.woff2",
+});
+
+const cantoRoman = localFont({ src: "../../fonts/canto/Canto-Roman.woff2" });
+
+const cantoSemiBold = localFont({
+  src: "../../fonts/canto/Canto-SemiBold.woff2",
+});
+
+const cantoSemiBoldItalic = localFont({
+  src: "../../fonts/canto/Canto-SemiBoldItalic.woff2",
+});
+
+export {
+  cantoBold,
+  cantoBoldItalic,
+  cantoItalic,
+  cantoLight,
+  cantoLightItalic,
+  cantoRoman,
+  cantoSemiBold,
+  cantoSemiBoldItalic,
+};
