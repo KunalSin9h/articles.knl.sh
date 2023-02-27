42db346646d2713daf9ef0c2a312c5d7a43d6dc2
Kunal Singh <kunalsin9h@gmail.com>
2023-02-27 20:41:23 +0530

    fix: missing back-to-home button

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,17 @@
 import ArticleShow from "../../components/ArticlesShow";
+import Link from "next/link";
 
 export default function ArticlePage() {
-  return <ArticleShow isPrev={false} />;
+  return (
+    <div className="mt-5">
+      <div className="ml-8 md:ml-10">
+        <Link href="/">
+          <button className="mb-2 underline-offset-8 hover:underline">
+            &larr; Back to home
+          </button>
+        </Link>
+      </div>
+      <ArticleShow isPrev={false} />
+    </div>
+  );
 }
