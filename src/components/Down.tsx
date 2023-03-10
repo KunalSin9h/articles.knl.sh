3e0f4208d1f55ea051537aaee3fd7c83734644d6
Kunal Singh <kunalsin9h@gmail.com>
2023-03-10 10:27:39 +0530

    update Down.txs

--- src/components/Down.tsx
+++ src/components/Down.tsx
@@ -2,7 +2,7 @@ export default function Down() {
   return (
     <p className="flex h-96 items-center justify-center p-10 text-center text-xl md:text-2xl">
       Dear visitors, please note that this website will be down every night from
-      24:00 to 06:00 IST (GMT+5:30) as part of our cost-saving measures. We
+      24:00 to 07:30 IST (GMT+5:30) as part of our cost-saving measures. We
       apologize for any inconvenience this may cause. Our website will be
       available again in the morning. Thank you for your understanding.
     </p>
