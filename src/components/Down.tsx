9e2004e82cdff4a186b6772173b6115b7578d05a
Kunal Singh <kunalsin9h@gmail.com>
2023-03-16 14:03:49 +0530

    update Down.txs

--- src/components/Down.tsx
+++ src/components/Down.tsx
@@ -5,6 +5,9 @@ export default function Down() {
       24:00 to 07:30 IST (GMT+5:30) as part of our cost-saving measures. We
       apologize for any inconvenience this may cause. Our website will be
       available again in the morning. Thank you for your understanding.
+      <br />
+      If, however, you are seeing this during the daytime, then there is
+      probably something I messed up. It will be fixed shortly.
     </p>
   );
 }
